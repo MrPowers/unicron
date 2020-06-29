ef5d67eb731f1f7beae983be2cded0438892ed25
MrPowers <matthewkevinpowers@gmail.com>
2020-06-29 16:39:21 -0500

    fix dag whitespace

--- unicron/dag.py
+++ unicron/dag.py
@@ -33,6 +33,7 @@ class DAG(object):
         """ Construct a new DAG with no nodes or edges. """
         self.reset_graph()
 
+
     def add_node(self, node_name, graph=None):
         """ Add a node if it does not exist yet, or error out. """
         if not graph:
@@ -41,11 +42,13 @@ class DAG(object):
             raise KeyError('node %s already exists' % node_name)
         graph[node_name] = set()
 
+
     def add_node_if_not_exists(self, node_name, graph=None):
         try:
             self.add_node(node_name, graph=graph)
         except KeyError:
             pass
+
 
     def delete_node(self, node_name, graph=None):
         """ Deletes this node and all edges referencing it. """
@@ -54,16 +57,17 @@ class DAG(object):
         if node_name not in graph:
             raise KeyError('node %s does not exist' % node_name)
         graph.pop(node_name)
-
         for node, edges in six.iteritems(graph):
             if node_name in edges:
                 edges.remove(node_name)
+
 
     def delete_node_if_exists(self, node_name, graph=None):
         try:
             self.delete_node(node_name, graph=graph)
         except KeyError:
             pass
+
 
     def add_edge(self, ind_node, dep_node, graph=None):
         """ Add an edge (dependency) between the specified nodes. """
@@ -79,6 +83,7 @@ class DAG(object):
         else:
             raise DAGValidationError()
 
+
     def delete_edge(self, ind_node, dep_node, graph=None):
         """ Delete an edge from the graph. """
         if not graph:
@@ -87,26 +92,27 @@ class DAG(object):
             raise KeyError('this edge does not exist in graph')
         graph[ind_node].remove(dep_node)
 
+
     def rename_edges(self, old_task_name, new_task_name, graph=None):
         """ Change references to a task in existing edges. """
         if not graph:
             graph = self.graph
         for node, edges in graph.items():
-
             if node == old_task_name:
                 graph[new_task_name] = copy(edges)
                 del graph[old_task_name]
-
             else:
                 if old_task_name in edges:
                     edges.remove(old_task_name)
                     edges.add(new_task_name)
+
 
     def predecessors(self, node, graph=None):
         """ Returns a list of all predecessors of the given node """
         if graph is None:
             graph = self.graph
         return [key for key in graph if node in graph[key]]
+
 
     def downstream(self, node, graph=None):
         """ Returns a list of all nodes this node has edges towards. """
@@ -115,6 +121,7 @@ class DAG(object):
         if node not in graph:
             raise KeyError('node %s is not in graph' % node)
         return list(graph[node])
+
 
     def all_downstreams(self, node, graph=None):
         """Returns a list of all nodes ultimately downstream
@@ -139,18 +146,18 @@ class DAG(object):
             )
         )
 
+
     def all_leaves(self, graph=None):
         """ Return a list of all leaves (nodes with no downstreams) """
         if graph is None:
             graph = self.graph
         return [key for key in graph if not graph[key]]
 
+
     def from_dict(self, graph_dict):
         """ Reset the graph and build it from the passed dictionary.
-
         The dictionary takes the form of {node_name: [directed edges]}
         """
-
         self.reset_graph()
         for new_node in six.iterkeys(graph_dict):
             self.add_node(new_node)
@@ -160,19 +167,21 @@ class DAG(object):
             for dep_node in dep_nodes:
                 self.add_edge(ind_node, dep_node)
 
+
     def reset_graph(self):
         """ Restore the graph to an empty state. """
         self.graph = OrderedDict()
+
 
     def independent_nodes(self, graph=None):
         """ Returns a list of all nodes in the graph with no dependencies. """
         if graph is None:
             graph = self.graph
-
         dependent_nodes = set(
             node for dependents in six.itervalues(graph) for node in dependents
         )
         return [node for node in graph.keys() if node not in dependent_nodes]
+
 
     def validate(self, graph=None):
         """ Returns (Boolean, message) of whether DAG is valid. """
@@ -185,27 +194,23 @@ class DAG(object):
             return (False, 'failed topological sort')
         return (True, 'valid')
 
+
     def topological_sort(self, graph=None):
         """ Returns a topological ordering of the DAG.
-
         Raises an error if this is not possible (graph is not valid).
         """
         if graph is None:
             graph = self.graph
-
         in_degree = {}
         for u in graph:
             in_degree[u] = 0
-
         for u in graph:
             for v in graph[u]:
                 in_degree[v] += 1
-
         queue = deque()
         for u in in_degree:
             if in_degree[u] == 0:
                 queue.appendleft(u)
-
         l = []
         while queue:
             u = queue.pop()
@@ -214,7 +219,6 @@ class DAG(object):
                 in_degree[v] -= 1
                 if in_degree[v] == 0:
                     queue.appendleft(v)
-
         if len(l) == len(graph):
             return l
         else:
