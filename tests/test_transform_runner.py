8b2bc29a1eb43da2f1e0226aed6681bd9738193e
MrPowers <matthewkevinpowers@gmail.com>
2020-07-18 18:48:44 -0500

    Optionally skip transformations if the columns they'll add is already present

--- tests/test_transform_runner.py
+++ tests/test_transform_runner.py
@@ -41,6 +41,26 @@ def test_simulate_shortest_path():
     assert actual == expected
 
 
+def test_simulate_shortest_path_with_skip():
+    data = [("jose", "jose"), ("li", "li"), ("luisa", "laura")]
+    df = spark.createDataFrame(data, ["name", "col_ab"])
+    graph = nx.DiGraph()
+    graph.add_edges_from([(ct_a, ct_ab), (ct_ab, ct_abc)])
+    actual = unicron.simulate_shortest_path(df, graph, ct_a, ct_abc, True)
+    expected = {"cols_added": ["col_abc", "col_a"], "cols_removed": []}
+    assert actual == expected
+
+
+def test_transforms_to_run():
+    data = [("jose",), ("li",), ("luisa",)]
+    df = spark.createDataFrame(data, ["name"])
+    graph = nx.DiGraph()
+    graph.add_edges_from([(ct_a, ct_ab), (ct_ab, ct_abc)])
+    actual = unicron.transforms_to_run(df, graph, ct_a, ct_abc, False)
+    expected = [ct_a, ct_ab, ct_abc]
+    assert actual == expected
+
+
 def test_run_custom_transforms():
     data = [("jose", "jose"), ("li", "li"), ("luisa", "laura")]
     df = spark.createDataFrame(data, ["name", "expected_name"])
--- unicron/transform_runner.py
+++ unicron/transform_runner.py
@@ -6,14 +6,35 @@ def transform_shortest_path(df, graph, start, end):
     return run_custom_transforms(df, transforms)
 
 
-def simulate_shortest_path(df, graph, start, end):
+def simulate_shortest_path(df, graph, start, end, skip_when_possible = False):
     transforms = nx.shortest_path(graph, start, end)
     cols_added = []
     cols_removed = []
+    df_columns = df.columns
     for ct in transforms:
-        cols_added = ct.cols_added + cols_added
-        cols_removed = ct.cols_removed + cols_removed
+        if skip_when_possible:
+            col_already_present = list(map(lambda col_name: col_name not in df_columns, ct.cols_added))
+            if all(col_already_present):
+                cols_added = ct.cols_added + cols_added
+                cols_removed = ct.cols_removed + cols_removed
+        else:
+            cols_added = ct.cols_added + cols_added
+            cols_removed = ct.cols_removed + cols_removed
     return {"cols_added": cols_added, "cols_removed": cols_removed}
+
+
+def transforms_to_run(df, graph, start, end, skip_when_possible = False):
+    transforms = nx.shortest_path(graph, start, end)
+    if skip_when_possible == False:
+        return transforms
+    else:
+        res = []
+        df_columns = df.columns
+        for ct in transforms:
+            col_already_present = list(map(lambda col_name: col_name not in df_columns, ct.cols_added))
+            if all(col_already_present):
+                res = res ++ ct
+        return res
 
 
 def run_custom_transforms(df, custom_transforms):
